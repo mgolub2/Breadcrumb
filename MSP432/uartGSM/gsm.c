92a48ef452bb0b054e48e6d4053a561cc62bf577
David Orchard <dlo9@uw.edu>
2016-03-08 17:30:51 -0800

    GSM interface now has hard-coded timeout

--- MSP432/uartGSM/gsm.c
+++ MSP432/uartGSM/gsm.c
@@ -38,9 +38,11 @@
 #include <xdc/std.h>
 #include <xdc/cfg/global.h>
 #include <xdc/runtime/System.h>
+#include <xdc/runtime/Error.h>
 
 /* BIOS Header files */
 #include <ti/sysbios/BIOS.h>
+#include <ti/sysbios/hal/Timer.h>
 
 /* TI-RTOS Header files */
 #include <ti/drivers/GPIO.h>
@@ -61,12 +63,15 @@
 #define CR_C '\015'
 #define LF_C '\012'
 
+// constrant string macro for lazy people not wanting to count the length
+#define CSTR(str) str, sizeof(str)-1
+
 void pcReadUARTCallback(UART_Handle handle, void *buf, size_t count) {
 	if (count != UART_ERROR && count > 0) {
 		// Write the data to the GSM
-		UART_write(uartGSM, buf, count);
+		UART_writePolling(uartGSM, buf, count);
 		// Write the data to the PC too
-		UART_write(uartPC, buf, count);
+		UART_writePolling(uartPC, buf, count);
 	}
 }
 
@@ -93,11 +98,41 @@ void clearGSMResponse() {
 	resTrunc = 0;
 }
 
+#define GSM_READ_TIMEOUT_MS 5000
+Timer_Handle gsmReadTimer;
+volatile int gsmReadTimeout = 0;
+void gsmReadTimeoutIsr(UArg arg) {
+	if (gsmReadTimeout < GSM_READ_TIMEOUT_MS)
+		gsmReadTimeout++;
+}
+
+inline int gsmReadTimedout() {
+	return gsmReadTimeout >= GSM_READ_TIMEOUT_MS;
+}
+
+inline void gsmReadTimeoutReset() {
+	gsmReadTimeout = 0;
+}
+
 // res MUST have length = GSM_MAX_RES_LEN
 void waitForGSMResponse(char *res) {
 	char input;
-	while (!gsmResponseReady())
-			UART_read(uartGSM, &input, 1);
+
+	Timer_start(gsmReadTimer);
+	while (!gsmResponseReady()) {
+		if (gsmReadTimedout()) {
+			Timer_stop(gsmReadTimer);
+			gsmReadTimeoutReset();
+			clearGSMResponse();
+			if (res != NULL)
+				res[0] = '\0';
+
+			UART_writePolling(uartPC, CSTR("GSM read timeout\r\n"));
+			break;
+		}
+		UART_read(uartGSM, &input, 1);
+	}
+	Timer_stop(gsmReadTimer);
 
 	if (res != NULL)
 		strcpy(res, (const char *)gsmRes);
@@ -157,7 +192,7 @@ void parseGSMResponse(void *buf, size_t count) {
 void gsmReadUARTCallback(UART_Handle handle, void *buf, size_t count) {
 	if (count != UART_ERROR && count > 0) {
 		// Write the data to the PC
-		UART_write(uartPC, buf, count);
+		UART_writePolling(uartPC, buf, count);
 		parseGSMResponse(buf, count);
 	}
 }
@@ -200,10 +235,10 @@ void setupUART() {
 
 // send a commant to the GSM module
 void cmdGSM(char *cmd, unsigned cmdlen) {
-	UART_write(uartPC, cmd, cmdlen);
-	UART_write(uartPC, "\r\n", 2);
-	UART_write(uartGSM, cmd, cmdlen);
-	UART_write(uartGSM, CR, 1);
+	UART_writePolling(uartPC, cmd, cmdlen);
+	UART_writePolling(uartPC, "\r\n", 2);
+	UART_writePolling(uartGSM, cmd, cmdlen);
+	UART_writePolling(uartGSM, CR, 1);
 }
 
 #define EAT_OK 1
@@ -222,63 +257,94 @@ int cmdVerifyResponse(char *cmd, unsigned cmdlen, char *expect, unsigned explen,
 	return strcmp(res, expect);
 }
 
-// constrant string macro for lazy people not wanting to count the length
-#define CSTR(str) str, sizeof(str)-1
 
 #define CMD_GSM_VERIFY(cmd, expect, eatOK) cmdVerifyResponse(CSTR(cmd), CSTR(expect), eatOK)
 
 // cmd, expect, and error_print must be const strings
 #define CMD_GSM_RET_ON_FAILURE(cmd, expect, eatOK, error_print) \
 	if (CMD_GSM_VERIFY(cmd, expect, eatOK)) { \
-		UART_write(uartPC, CSTR(cmd)); \
+		UART_writePolling(uartPC, CSTR(error_print)); \
 		return -1; \
 	}
 
 int setupGSM() {
 	// TODO: check for +SIMD: 11 and +SIMD: 4, but must have a timeout
-	UART_write(uartPC, CSTR("Initializing GSM Connection\r\n"));
+	UART_writePolling(uartPC, CSTR("Initializing GSM Connection\r\n"));
 
-	UART_write(uartPC, CSTR("Verifying GPRS attachment\r\n"));
+	UART_writePolling(uartPC, CSTR("Verifying GPRS attachment\r\n"));
 	if (CMD_GSM_VERIFY("AT+CGATT?", "+CGATT: 1", EAT_OK)) {
 		// Cell not attached
-		UART_write(uartPC, CSTR("Attaching GPRS"));
-		CMD_GSM_RET_ON_FAILURE("AT+CGATT=1", "OK", NO_EAT_OK, "Failed to attach GPRS");
+		UART_writePolling(uartPC, CSTR("Not attached, attaching GPRS now\r\n"));
+		CMD_GSM_RET_ON_FAILURE("AT+CGATT=1", "OK", NO_EAT_OK, "Failed to attach GPRS\r\n");
 	}
 
-	UART_write(uartPC, CSTR("Verifying network connection\r\n"));
-	CMD_GSM_RET_ON_FAILURE("AT+CREG?", "+CREG: 0,1", EAT_OK, "Not registered with network");
+	UART_writePolling(uartPC, CSTR("Verifying network connection\r\n"));
+	CMD_GSM_RET_ON_FAILURE("AT+CREG?", "+CREG: 0,1", EAT_OK, "Not registered with network\r\n");
 
-	UART_write(uartPC, CSTR("Setting PDP context\r\n"));
-	CMD_GSM_RET_ON_FAILURE("AT+CGDCONT=1,\"IP\",\"breadnet\"", "OK", NO_EAT_OK, "Failed to set PDP context");
-	CMD_GSM_RET_ON_FAILURE("AT+CGACT=1,1", "OK", NO_EAT_OK, "Failed to activate PDP context");
+	UART_writePolling(uartPC, CSTR("Setting PDP context\r\n"));
+	CMD_GSM_RET_ON_FAILURE("AT+CGDCONT=1,\"IP\",\"breadnet\"", "OK", NO_EAT_OK, "Failed to set PDP context\r\n");
+	CMD_GSM_RET_ON_FAILURE("AT+CGACT=1,1", "OK", NO_EAT_OK, "Failed to activate PDP context\r\n");
 
-	UART_write(uartPC, CSTR("GSM Setup Finished\r\n"));
+	UART_writePolling(uartPC, CSTR("GSM Setup Finished\r\n"));
 
 	return 0;
 }
 
+int setupTimer() {
+	Timer_Params timerParams;
+	Error_Block eb;
+
+	Error_init(&eb);
+	Timer_Params_init(&timerParams);
+
+	// 1ms timeout
+	timerParams.startMode = Timer_StartMode_USER;
+	timerParams.period = 1000;
+	timerParams.periodType = Timer_PeriodType_MICROSECS;
+	timerParams.arg = 1;
+
+	gsmReadTimer = Timer_create(Timer_ANY, gsmReadTimeoutIsr, &timerParams, &eb);
+
+	return gsmReadTimer == NULL;
+}
+
 #define DATALEN "3"
 #define DATA "GET"
-void requestGET() {
-//	CMD_GSM_CHECK("AT+SDATACONF=1,\"TCP\",\"www.google.com\",80", "OK"); // Configure connection
-//	CMD_GSM_CHECK("AT+SDATASTART=1,1", "OK");  // Start connection
+int requestGET() {
+	UART_writePolling(uartPC, CSTR("Connecting to Google\r\n"));
+	CMD_GSM_RET_ON_FAILURE("AT+SDATACONF=1,\"TCP\",\"www.google.com\",80", "OK", NO_EAT_OK, "TCP configuration failed\r\n");
+	CMD_GSM_RET_ON_FAILURE("AT+SDATASTART=1,1", "OK", NO_EAT_OK, "TCP connect failed\r\n");
 //	// TODO: Check the socket AT+SDATASTATUS=1
-//	CMD_GSM_CHECK("AT+SDATATSEND=1," DATALEN, ">");  // Issue the request
-//	CMD_GSM_CHECK(DATA CTRLZ, "OK");  // Enter the data
+	CMD_GSM_RET_ON_FAILURE("AT+SDATATSEND=1," DATALEN, ">", NO_EAT_OK, "Request to GSM failed\r\n");
+	CMD_GSM_RET_ON_FAILURE(DATA CTRLZ, "OK", NO_EAT_OK, "Entering data failed\r\n");
+
+	return 0;
 }
 
 void gsmTask(UArg arg0, UArg arg1) {
 	char input;
 
+	//System_printf("test");
+	//System_flush();
+
+	int timerFail = setupTimer();
 	setupUART();
-	setupGSM();
-	requestGET();
+	if (timerFail)
+		UART_writePolling(uartPC, CSTR("Failed to create timer\r\n"));
+	if (setupGSM())
+		UART_writePolling(uartPC, CSTR("Failed to setup GSM\r\n"));
+	if (requestGET())
+		UART_writePolling(uartPC, CSTR("GET attempt failed\r\n"));
 
 	while (1) {
 		// Read from all the UARTS
 		UART_read(uartPC, &input, 1);
-		if (input == '{') {
-			setupGSM();
+		if (input == 's') {
+			if (setupGSM())
+				UART_writePolling(uartPC, CSTR("Failed to setup GSM\r\n"));
+		} else if (input == 'g') {
+			if (requestGET())
+				UART_writePolling(uartPC, CSTR("GET attempt failed\r\n"));
 		}
 		UART_read(uartGSM, &input, 1);
 	}
